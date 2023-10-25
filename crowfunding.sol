f40b3509a79490c1c69a0db444680d262b0c729e
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-10-25 16:51:15 -0300

    inicial commit

---
+++ crowfunding.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4;
+
+contract crowFunding{
+
+    struct Proyect{
+        uint Id;
+        string name;
+        uint targetAmount;
+        uint collectedAmount;
+    }
+    uint proyectsCount = 0;
+    mapping (string => Proyect) proyects;
+
+    //Allows to create a new proyect
+    function newProyect(string memory _name, uint _target) public{
+        proyectsCount++;
+        proyects[_name] = Proyect(proyectsCount,_name,_target, 0);
+    }
+
+    //Returns if target amount was reached
+    function targetAmountReached(string memory _name, uint _amount) private view returns (bool) {
+        Proyect memory proyect = proyects[_name];
+        return proyect.targetAmount < proyect.collectedAmount + _amount;
+    }
+
+    //Allows to fund an existing proyect
+    function fundProyect(string memory _name, uint _amount) public{
+        require(!targetAmountReached(_name, _amount),"TARGET AMOUNT REACHED");
+        proyects[_name].collectedAmount += _amount;
+    }
+}
