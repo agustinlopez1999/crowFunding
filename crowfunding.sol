04bb180a5900e70682e268ff0741062721ef9524
agustinlopez1999 <lopezagustin1999@hotmail.com>
2023-10-27 12:34:39 -0300

    public functions that are call externally defined as external

--- crowfunding.sol
+++ crowfunding.sol
@@ -13,7 +13,7 @@ contract crowFunding{
     mapping (string => Proyect) proyects;
 
     //Allows to create a new proyect
-    function newProyect(string memory _name, uint _target) public{
+    function newProyect(string memory _name, uint _target) external{
         proyectsCount++;
         proyects[_name] = Proyect(proyectsCount,_name,_target, 0);
     }
@@ -25,19 +25,19 @@ contract crowFunding{
     }
 
     //Returns amount left to reach target
-    function amountToReachTarget(string memory _name) public view returns (uint) {
+    function amountToReachTarget(string memory _name) external view returns (uint) {
         Proyect memory proyect = proyects[_name];
         return proyect.targetAmount - proyect.collectedAmount;
     }
 
     //Allows to fund an existing proyect
-    function fundProyect(string memory _name, uint _amount) public{
+    function fundProyect(string memory _name, uint _amount) external{
         require(!targetAmountReached(_name, _amount),"TARGET AMOUNT REACHED");
         proyects[_name].collectedAmount += _amount;
     }
 
     //Returns proyect name, target and collected
-    function proyectInfo(string memory _name) public view returns(uint, uint, uint){
+    function proyectInfo(string memory _name) external view returns(uint, uint, uint){
         Proyect memory proyect = proyects[_name];
         return (proyect.Id,proyect.targetAmount,proyect.collectedAmount); 
     }
